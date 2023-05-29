427fd5254dced7137a549c2032bce09f743b94f1
Thomas Petracco <git@mail.tompec.com>
2023-05-29 11:29:01 +0800

    Updated script to match only .zip TLDs

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ setInterval(() => {
     // This script adds a red border and some padding to all links with .zip TLD
     const links = document.getElementsByTagName('a');
     for (let link of links) {
-        // Extract hostname from link
-        let hostname = new URL(link.href).hostname;
+        // Check if the link has an href attribute
+        if (link.hasAttribute('href')) {
+            try {
+                // Extract hostname from link
+                let hostname = new URL(link.href).hostname;
 
-        // Check if the TLD is .zip
-        if (hostname.slice(-4) === '.zip') {
-            link.style.border = '2px solid red';
-            link.style.padding = '5px';
+                // Check if the URL ends with ".zip"
+                if (hostname.slice(-4) === '.zip') {
+                    link.style.border = '2px solid red';
+                    link.style.padding = '5px';
+                }
+            } catch (error) {
+                // Handle invalid URLs
+                console.error('Invalid URL:', link.href);
+            }
         }
     }
 }, 500);
